376fcf5e522154d212b614e58a84cf25530d3bf3
David Lambert <codepenguin@gmail.com>
2022-06-04 21:39:26 -0400

    WIP: Implemented photo transition

---
+++ src/components/PhotoComponent.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import { ref, watch } from "vue";
+
+const props = defineProps<{
+  url: string;
+}>();
+
+const photo = ref<HTMLImageElement>();
+const photoUrl = ref(props.url);
+const transitionPhoto = ref<HTMLImageElement>();
+const transitionPhotoUrl = ref(props.url);
+
+console.log(props.url);
+
+watch(
+  () => props.url,
+  (newValue, oldValue) => {
+    if (transitionPhoto.value?.classList.contains("opacity-100")) {
+      transitionPhotoUrl.value = oldValue;
+      photoUrl.value = newValue;
+      transitionPhoto.value?.classList.remove("opacity-100");
+      transitionPhoto.value?.classList.add("opacity-0");
+    } else {
+      transitionPhotoUrl.value = newValue;
+      photoUrl.value = oldValue;
+      transitionPhoto.value?.classList.remove("opacity-0");
+      transitionPhoto.value?.classList.add("opacity-100");
+    }
+  }
+);
+</script>
+
+<template>
+  <div class="relative w-full h-full overflow-hidden">
+    <img
+      :src="photoUrl"
+      ref="photo"
+      alt="Photo"
+      class="object-cover w-full h-full"
+    />
+    <img
+      :src="transitionPhotoUrl"
+      ref="transitionPhoto"
+      alt="Transition Photo"
+      class="absolute top-0 left-0 object-cover w-full h-full transition-opacity duration-3000 ease-in-out opacity-0"
+    />
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,17 @@
 <script setup lang="ts">
-import TheWelcome from "@/components/TheWelcome.vue";
+import PhotoComponent from "@/components/PhotoComponent.vue";
+import { ref } from "vue";
+
+const url = ref("hummingbird.jpg");
+
+function switchPhoto() {
+  url.value =
+    url.value === "hummingbird.jpg" ? "plates.jpg" : "hummingbird.jpg";
+}
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <PhotoComponent :url="url" @click="switchPhoto" />
   </main>
 </template>
