4a9461716d3783dcd65dad3cdad7a481d9047dc7
David Lambert <codepenguin@gmail.com>
2022-06-25 15:30:42 -0400

    Flatten build output to remove assets folder.

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    assetsDir: ''
+  },
   plugins: [vue()],
   resolve: {
     alias: {
